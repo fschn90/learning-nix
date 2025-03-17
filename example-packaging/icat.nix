69ff8297c80badb3bdaeb13389864b42ba5dd1ac
fschn90 <79014934+fschn90@users.noreply.github.com>
2025-03-17 18:53:43 +0100

    icat packaging attempt, fixed dependencies but build still failing

---
+++ example-packaging/icat.nix
@@ -0,0 +1,21 @@
+# hello.nix
+{ stdenv
+, fetchFromGitHub
+, imlib2
+, xorg
+,
+}:
+
+stdenv.mkDerivation {
+  pname = "icat";
+  version = "v0.5";
+
+  src = fetchFromGitHub {
+    owner = "atextor";
+    repo = "icat";
+    rev = "v0.5";
+    sha256 = "0wyy2ksxp95vnh71ybj1bbmqd5ggp13x3mk37pzr99ljs9awy8ka";
+  };
+
+  buildInputs = [ imlib2 xorg.libX11 ];
+}
