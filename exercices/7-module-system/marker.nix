a640ed19cd3e5e055fe98354c3c852fb42a36ad5
fschn90 <79014934+fschn90@users.noreply.github.com>
2025-05-28 14:24:02 +0200

    module system: Defining options in other modules

--- exercices/7-module-system/marker.nix
+++ exercices/7-module-system/marker.nix
@@ -16,4 +16,29 @@ in
       type = lib.types.listOf markerType;
     };
   };
+
+  config = {
+
+    map.markers = [
+      { location = "new york"; }
+    ];
+
+    map.center = lib.mkIf
+      (lib.length config.map.markers >= 1)
+      null;
+
+    map.zoom = lib.mkIf
+      (lib.length config.map.markers >= 2)
+      null;
+
+    requestParams =
+      let
+        paramForMarker =
+          builtins.map
+            (marker: "$(${config.scripts.geocode}/bin/geocode ${
+          lib.escapeShellArg marker.location})")
+            config.map.markers;
+      in
+      [ "markers=\"${lib.concatStringsSep "|" paramForMarker}\"" ];
+  };
 }
