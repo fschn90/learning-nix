29cd641daf29a873c4a29a3f1adce51156c0e12b
fschn90 <79014934+fschn90@users.noreply.github.com>
2025-05-28 14:06:27 +0200

    module system: the submodule type

---
+++ exercices/7-module-system/marker.nix
@@ -0,0 +1,19 @@
+{ pkgs, lib, config, ... }:
+let
+  markerType = lib.types.submodule {
+    options = {
+      location = lib.mkOption {
+        type = lib.types.nullOr lib.types.str;
+        default = null;
+      };
+    };
+  };
+in
+{
+
+  options = {
+    map.markers = lib.mkOption {
+      type = lib.types.listOf markerType;
+    };
+  };
+}
