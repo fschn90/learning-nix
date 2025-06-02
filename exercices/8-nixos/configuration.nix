648c92f1b6a96db40bf10bf670a65f36dd3ede61
fschn90 <79014934+fschn90@users.noreply.github.com>
2025-06-02 14:43:49 +0200

    NixOS virtual machines: Running GNOME on a graphical VM

--- exercices/8-nixos/configuration.nix
+++ exercices/8-nixos/configuration.nix
@@ -15,6 +15,13 @@
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
+  # Enable the X11 windowing system.
+  services.xserver.enable = true;
+
+  # Enable the GNOME Desktop Environment.
+  services.xserver.displayManager.gdm.enable = true;
+  services.xserver.desktopManager.gnome.enable = true;
+
   users.users.alice = {
     isNormalUser = true;
     extraGroups = [ "wheel" ];
