66a5c0f3fcabffda0b611c035f8b6c5c8c5b6dab
fschn90 <79014934+fschn90@users.noreply.github.com>
2025-06-21 15:43:58 +0200

    NixOS, Building a bootable ISO image, customized for zfs with fish

--- exercices/8-nixos/myimage.nix
+++ exercices/8-nixos/myimage.nix
@@ -1,11 +1,39 @@
-{ pkgs, modulesPath, lib, ... }: {
+{ config, pkgs, modulesPath, lib, ... }:
+
+
+let
+  zfsCompatibleKernelPackages = lib.filterAttrs
+    (
+      name: kernelPackages:
+        (builtins.match "linux_[0-9]+_[0-9]+" name) != null
+        && (builtins.tryEval kernelPackages).success
+        && (!kernelPackages.${config.boot.zfs.package.kernelModuleAttribute}.meta.broken)
+    )
+    pkgs.linuxKernel.packages;
+  latestKernelPackage = lib.last (
+    lib.sort (a: b: (lib.versionOlder a.kernel.version b.kernel.version)) (
+      builtins.attrValues zfsCompatibleKernelPackages
+    )
+  );
+in
+
+{
   imports = [
     "${modulesPath}/installer/cd-dvd/installation-cd-minimal.nix"
   ];
 
   # use the latest Linux kernel
-  boot.kernelPackages = pkgs.linuxPackages_latest;
+  # boot.kernelPackages = pkgs.linuxPackages_latest;
+  boot.kernelPackages = latestKernelPackage;
+
+
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
+  networking.hostId = "e2990a3c";
+
+  programs.fish.enable = true;
+  users.defaultUserShell = pkgs.fish;
 
   # Needed for https://github.com/NixOS/nixpkgs/issues/58959
-  boot.supportedFilesystems = lib.mkForce [ "btrfs" "reiserfs" "vfat" "f2fs" "xfs" "ntfs" "cifs" ];
+  boot.supportedFilesystems = lib.mkForce [ "btrfs" "reiserfs" "vfat" "f2fs" "xfs" "ntfs" "cifs" "zfs" ];
 }
